40a7c7ff3368152ce8932fdf03dc52d4b5c5c4d2
Rebecca Goveia <rebeccagoveia@gmail.com>
2017-12-19 22:42:04 -0700

    Added outdoor space schema and added db calls to the routes

--- app.js
+++ app.js
@@ -7,26 +7,52 @@ mongoose.connect("mongodb://localhost/outdoor_spaces");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var outdoorspaces = [
-		{name: "Webb Bridge Park", image: "https://farm1.staticflickr.com/17/93685750_30be6061a2.jpg"},
-		{name: "Rock Milton Park", image: "https://farm5.staticflickr.com/4585/37643820335_408da9b83e.jpg"},
-		{name: "Amicalolla Falls", image: "https://farm6.staticflickr.com/5463/10053135685_7711e59c85.jpg"}
-];
+var outdoorspaceSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
 
+var Outdoorspace = mongoose.model("Outdoorspace", outdoorspaceSchema);
+
+/*
+Outdoorspace.create(
+    {
+        name: "Rock Milton Park", 
+        image: "https://farm5.staticflickr.com/4585/37643820335_408da9b83e.jpg"
+    }, function(err, outdoorspace) {
+        if(err){
+            console.log(err);
+        } else {
+            console.log("Newly created outdoor space");
+            console.log(outdoorspace);
+        }
+    });
+*/
 app.get("/", function(req, res){
 	res.render("landing");
 })
 
 app.get("/outdoorspaces", function(req, res) {
-	res.render("outdoorspaces", {outdoorspaces: outdoorspaces});
+	Outdoorspace.find({}, function(err, allOutdoorspaces) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("outdoorspaces", {outdoorspaces: allOutdoorspaces});
+		}
+	});
 });
 
 app.post("/outdoorspaces", function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newOutdoorspace = {name: name, image: image};
-	outdoorspaces.push(newOutdoorspace);
-	res.redirect("/outdoorspaces");
+	Outdoorspace.create(newOutdoorspace, function(err, newlyCreated) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/outdoorspaces");
+		}
+	});
 });
 
 app.get("/outdoorspaces/new", function(req, res) {
